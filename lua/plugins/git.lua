772839642fb501f4f73cf08914c7f35944c08b8a
Paul Rawcliffe <jpr999@gmail.com>
2025-08-12 14:36:01 +0100

    latest

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -66,12 +66,4 @@ return {
 			require("gitblame").setup({ enabled = false })
 		end,
 	},
-	{
-		"f-person/git-blame.nvim",
-		event = "BufRead",
-		config = function()
-			vim.cmd("highlight default link gitblame SpecialComment")
-			require("gitblame").setup({ enabled = false })
-		end,
-	},
 }
