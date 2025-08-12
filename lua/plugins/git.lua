f6c3f02ed2d974638ef3828ac310242e487922d7
Paul Rawcliffe <jpr999@gmail.com>
2025-08-12 03:22:46 +0100

    changed to neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,39 +1,19 @@
 return {
 	-- for all git plugins
 	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
 
-			local myFugitive = vim.api.nvim_create_augroup("myFugitive", {})
-
-			local autocmd = vim.api.nvim_create_autocmd
-			autocmd("BufWinEnter", {
-				group = myFugitive,
-				pattern = "*",
-				callback = function()
-					if vim.bo.ft ~= "fugitive" then
-						return
-					end
-
-					local bufnr = vim.api.nvim_get_current_buf()
-					local opts = { buffer = bufnr, remap = false }
-
-					vim.keymap.set("n", "<leader>P", function()
-						vim.cmd.Git("push")
-					end, opts)
-
-					-- NOTE: rebase always
-					vim.keymap.set("n", "<leader>p", function()
-						vim.cmd.Git({ "pull", "--rebase" })
-					end, opts)
-
-					-- NOTE: easy set up branch that wasn't setup properly
-					vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
-				end,
-			})
-		end,
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+			"folke/snacks.nvim", -- optional
+		},
 	},
+
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
