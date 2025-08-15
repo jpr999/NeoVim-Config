c621ebd24481a433e0c5dcce548f70e4fdda68ef
Paul Rawcliffe <jpr999@gmail.com>
2025-08-15 23:59:23 +0100

    Latest

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,11 +1,18 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    --    "catppuccin/nvim",
+    --    name = "catppuccin",
+    --    priority = 1000,
+    --    opts = {},
+    --    config = function()
+    --      vim.cmd [[colorscheme catppuccin-mocha]]
+    --    end
+    "wnkz/monoglow.nvim",
+    lazy = false,
     priority = 1000,
     opts = {},
     config = function()
-      vim.cmd [[colorscheme catppuccin-mocha]]
+      vim.cmd [[colo monoglow-z]]
     end
   }
 }
