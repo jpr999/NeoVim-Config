f03883f9f705af67c797265b5dc913c783ed285d
Paul Rawcliffe <jpr999@gmail.com>
2025-08-15 08:09:10 +0100

    Latest

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,68 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-			{ "folke/lazydev.nvim", opts = {} },
-		},
-		opts = {
-			servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				},
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      { "folke/lazydev.nvim", opts = {} },
+    },
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
 
-				--ts_ls = {
-				--  on_attach = function(client, bufnr)
-				--    require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-				--  end,
-				--},
-				ts_ls = {},
-				eslint = {},
-				tailwindcss = {},
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup()
+        ts_ls = {},
+        eslint = {},
+        tailwindcss = {},
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup()
 
-			require("mason-lspconfig").setup({
-				ensure_installed = { "clangd", "lua_ls", "eslint", "tailwindcss", "tinymist", "ts_ls" },
-			})
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "eslint", "tailwindcss", "tinymist", "ts_ls" },
+      })
 
-			for server, config in pairs(opts.servers) do
-				vim.lsp.config(server, config)
-				vim.lsp.enable(server)
-			end
-		end,
-	},
+      for server, config in pairs(opts.servers) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+    end,
+
+    -- Diagnostics
+vim.diagnostic.config({
+    virtual_lines = true,
+    -- virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+    })
+
+
+
+
+  },
 }
